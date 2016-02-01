d204782003e12b1b85631f965776062a1aa27f92
lburgazzoli <lburgazzoli@gmail.com>
2016-02-01 17:59:58 +0100

    updates

--- apps/squid/docker-run.sh
+++ apps/squid/docker-run.sh
@@ -1,7 +1,7 @@
-docker run \
+sudo docker run \
     -ti \
     --rm \
     --name squid \
     -p 3128:3128 \
     lburgazzoli/app-squid \
-    /usr/sbin/squid3 -N -d 0
+    /usr/sbin/squid -N -d 0
