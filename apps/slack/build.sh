ee6ff4ce39eaab0d182193cfe143ba40f34d4c09
lburgazzoli <lburgazzoli@gmail.com>
2016-04-13 17:06:57 +0200

    update slack

--- apps/slack/build.sh
+++ apps/slack/build.sh
@@ -2,8 +2,8 @@
 
 
 sudo docker build \
-    --tag="lburgazzoli/app-slack:1.2.6" \
+    --tag="lburgazzoli/app-slack:2.0.3" \
     --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
     --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
-    --build-arg APP_VERSION=1.2.6 \
+    --build-arg APP_VERSION=2.0.3 \
     .
