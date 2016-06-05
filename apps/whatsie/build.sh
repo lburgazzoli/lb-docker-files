bbcd9c01cce2c9183386c468b397822ed49ae735
lburgazzoli <lburgazzoli@gmail.com>
2016-06-05 08:53:13 +0200

    updates

---
+++ apps/whatsie/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo docker build \
+    --tag="lburgazzoli/app-whatsie:${APP_VERSION}" \
+    --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
+    --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
+    .
