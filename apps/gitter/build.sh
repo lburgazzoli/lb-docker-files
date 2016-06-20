310401859f283ce925744cd13c34d3aaf4385f33
lburgazzoli <lburgazzoli@gmail.com>
2016-06-20 16:54:19 +0200

    add gitter

---
+++ apps/gitter/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo docker build \
+    --tag="lburgazzoli/app-gitter" \
+    --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
+    --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
+    --build-arg APP_VERSION=3.0.3 \
+    .
