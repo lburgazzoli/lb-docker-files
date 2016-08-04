6cbf0d2b0336c8cca0c733c724e46e34671680f0
lburgazzoli <lburgazzoli@gmail.com>
2016-08-04 18:13:51 +0200

    weechat

--- apps/weechat/build.sh
+++ apps/weechat/build.sh
@@ -2,7 +2,7 @@
 
 
 sudo docker build \
-    --tag="lburgazzoli/app-chrome" \
+    --tag="lburgazzoli/app-weechat" \
     --build-arg DOCKER_USER=$LOGNAME \
     --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
     --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
