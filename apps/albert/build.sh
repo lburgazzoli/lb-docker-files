8b58bdc5388f8c33622fbd5a0412db6c120fda7e
lburgazzoli <lburgazzoli@gmail.com>
2016-08-25 08:43:17 +0200

    cleanup

--- apps/albert/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-sudo docker build \
-    --tag="lburgazzoli/app-albert" \
-    --build-arg DOCKER_USER=$LOGNAME \
-    --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
-    --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
-    .
