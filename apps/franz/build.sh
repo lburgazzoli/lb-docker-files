7b2622a10f098297ddbef483d975f04e6ab23d7f
lburgazzoli <lburgazzoli@gmail.com>
2016-09-10 15:04:05 +0200

    franz

--- apps/franz/build.sh
+++ apps/franz/build.sh
@@ -6,6 +6,6 @@ sudo docker build \
     --build-arg DOCKER_USER=$LOGNAME \
     --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
     --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
-    --build-arg APP_NAME=frranz \
-    --build-arg APP_VERSION=3.1.1 \
+    --build-arg APP_NAME=franz \
+    --build-arg APP_VERSION=4.0.4 \
     .
