85eb125db591e3752cd56aaf92f7fa2f4a43b779
lburgazzoli <lburgazzoli@gmail.com>
2016-10-13 16:16:17 +0200

    vscode

--- apps/vscode/build.sh
+++ apps/vscode/build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 APP_NAME=vscode
-APP_VERSION=1.5.2
 
 docker build \
     --tag="lburgazzoli/app-${APP_NAME}" \
     --build-arg APP_NAME=$APP_NAME \
-    --build-arg APP_VERSION=$APP_VERSION \
     --build-arg DOCKER_USER=$LOGNAME \
     --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
     --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
