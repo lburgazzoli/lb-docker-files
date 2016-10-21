c557b26c7aed14a706287917fdb7587c4783372b
lburgazzoli <lburgazzoli@gmail.com>
2016-10-21 18:09:17 +0200

    cleanup fedora

--- fedora/23-dev/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker build \
-    --tag="lburgazzoli/fedora-dev:23" \
-    .
--- fedora/23/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker build \
-    --tag="lburgazzoli/fedora:23" \
-    .
