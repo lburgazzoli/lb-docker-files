261ba47308a06424e5efc14c8fd1167879e9874e
lburgazzoli <lburgazzoli@gmail.com>
2017-03-14 12:31:07 +0100

    bitlbee

---
+++ apps/weechat/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+WEECHAT_VERSION=1.7
+
+docker build \
+    --tag="lburgazzoli/app-weechat" \
+    --build-arg DOCKER_USER=$LOGNAME \
+    --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
+    --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
+    --build-arg APP_NAME=weechat \
+    --build-arg APP_VERSION=${WEECHAT_VERSION} \
+    .
