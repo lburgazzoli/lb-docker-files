328dbaff7d9d38a0f64b66d4ec7879a0d573348a
lburgazzoli <lburgazzoli@gmail.com>
2017-03-17 12:44:02 +0100

    audacious

---
+++ apps/audacious/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+docker build \
+    --tag="lburgazzoli/app-audacious" \
+    --build-arg DOCKER_USER=$LOGNAME \
+    --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
+    --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
+    .
