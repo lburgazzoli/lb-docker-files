4a1769f73ae883afc3946a22c8c66b63806f0800
lburgazzoli <lburgazzoli@gmail.com>
2017-06-21 10:40:12 +0200

    Upgrade to Calibre 3.0

--- apps/calibre/build.sh
+++ apps/calibre/build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+APP_GROUP=lburgazzoli
+APP_NAME=atom
+
+if [ "$#" -eq 1 ]; then
+    if [[ "$1" == "-f" ]]; then
+    docker image rm ${APP_GROUP}/app-${APP_NAME}
+    fi
+fi
 
 docker build \
     --tag="lburgazzoli/app-calibre" \
