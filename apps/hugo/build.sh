b48d57693102b59633ee1524fa9ba03825b5f1b5
lburgazzoli <lburgazzoli@gmail.com>
2017-08-17 08:21:58 +0200

    hugo

---
+++ apps/hugo/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+APP_GROUP=lburgazzoli
+APP_NAME=hugo
+
+if [ "$#" -eq 1 ]; then
+    if [[ "$1" == "-f" ]]; then
+    docker image rm ${APP_GROUP}/app-${APP_NAME}
+    fi
+fi
+
+docker build \
+    --no-cache \
+    --tag="lburgazzoli/app-hugo" \
+    --build-arg DOCKER_USER=$LOGNAME \
+    --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
+    --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
+    .
