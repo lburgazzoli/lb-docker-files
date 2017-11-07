16bc7711dbedae8d2dae88810842975cc292a7d6
lburgazzoli <lburgazzoli@gmail.com>
2017-11-07 14:58:01 +0100

    franz

--- apps/franz/build.sh
+++ apps/franz/build.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+APP_GROUP=lburgazzoli
+APP_NAME=franz
+REPO_OWNER="meetfranz"
+REPO_NAME="franz"
+BINARY_NAME="x86_64.AppImage"
+
+if [ "$#" -eq 1 ]; then
+    if [[ "$1" == "-f" ]]; then
+    docker image rm ${APP_GROUP}/app-${APP_NAME}
+    fi
+fi
+
+URL=$(curl -s https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/latest \
+    | jq --raw-output \
+        ".assets[] | select(.name | endswith(\"${BINARY_NAME}\")) | .browser_download_url")
+
 
 docker build \
     --tag="lburgazzoli/app-franz" \
@@ -7,5 +23,5 @@ docker build \
     --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
     --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
     --build-arg APP_NAME=franz \
-    --build-arg APP_VERSION=4.0.4 \
+    --build-arg APP_URL=$URL \
     .
