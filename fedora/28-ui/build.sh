88d7d9767e4a48edf66cdd0ae16739c90330b5e1
lburgazzoli <lburgazzoli@gmail.com>
2018-05-03 16:59:00 +0200

    updates

--- fedora/28-ui/build.sh
+++ fedora/28-ui/build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-WEECHAT_VERSION=2.0.1
-
 docker build \
-    --tag="lburgazzoli/app-weechat" \
+    --tag="lburgazzoli/fedora-ui:28" \
     --build-arg DOCKER_USER=$LOGNAME \
     --build-arg DOCKER_USER_GID=$(id $LOGNAME -g) \
     --build-arg DOCKER_USER_UID=$(id $LOGNAME -u) \
-    --build-arg APP_NAME=weechat \
-    --build-arg APP_VERSION=${WEECHAT_VERSION} \
     .
